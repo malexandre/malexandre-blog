4604bda9028cd3a9f3c9827cde2f0960b2aa8f5e
Marc Alexandre <github@malexandre.fr>
2017-09-02 19:40:40 +0200

    Manage hugo via gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,13 @@
+var del = require("del");
 const gulp = require("gulp");
 const plugins = require("gulp-load-plugins")();
+
+const clean = () => {
+  return del[
+    './public',
+    './static/img/'
+  ]
+}
 
 const css = () => {
   gulp
@@ -24,11 +32,18 @@ const img = () => {
     .pipe(gulp.dest("static/img/"));
 };
 
+const hugoBuild = () => plugins.sh;
+
+gulp.task("clean", clean);
 gulp.task("css", css);
 gulp.task("img", img);
-gulp.task("watchers", ["css", "img"], () => {
+
+gulp.task("build", plugins.sequence('clean', ["css", "img"]));
+gulp.task("watchers", ["build"], () => {
   gulp.watch("assets/css/**/*.css", ["css"]);
   gulp.watch("assets/img/**/*", ["img"]);
 });
 
-gulp.task("default", ["watchers"]);
+gulp.task("hugo:dev", ["watchers"], plugins.shell.task(["hugo server"]));
+gulp.task("hugo:build", ["build"], plugins.shell.task(["hugo"]));
+gulp.task("default", ["hugo:dev"]);
